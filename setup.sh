2fa49694e5e1e7b3f3bff769ddcaf11b0277bdde
Iurii Nartov <yurahttp@gmail.com>
2025-10-06 17:58:13 +0300

    Updating setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Input parameters:
 #   - $1: the module name to run tests, currently supported (api, web)
-#   - $2: the path to the project ( !!! excluding the project folder name !!! ), defaults to $DEFAULT_REPO_PATH
+#   - $2: the path to the project, defaults to $DEFAULT_REPO_PATH
 # Exported variables: HOST_ARTIFACTS, ROOT_VENV, TEST_VENV, COPIED_PROJECT_PATH
 
 if [ -z $1 ]; then
@@ -10,8 +10,7 @@ if [ -z $1 ]; then
   return 1
 fi
 
-DEFAULT_PROJECT_NAME="python-automation-home-test"
-DEFAULT_REPO_PATH="$HOME/$DEFAULT_PROJECT_NAME"
+DEFAULT_REPO_PATH="$HOME/python-automation-home-test"
 
 # Repo path defaults to $DEFAULT_REPO_PATH
 if [[ -z "$2" ]]; then
@@ -26,19 +25,22 @@ elif [[ ! -d "$2" ]]; then
   return 1
 else
   if [[ "${2: -1}" == "/" ]]; then
-    REPO="$2$DEFAULT_PROJECT_NAME"
+    REPO="$2"
   else
-    REPO="$2/$DEFAULT_PROJECT_NAME"
+    REPO="$2"
   fi
-  echo "Using $REPO path for the repo"
+  echo "Using '$REPO' path for the repo"
 fi
+
+# Let's retrieve the project folder name from the path to the project
+PROJECT_FOLDER_NAME="${REPO##*/}"
 
 # path where workspace will be stored
 HOST_WORKSPACE="$HOME/TEST1/workspace"
 # path where artifacts will be stored
 HOST_ARTIFACTS="$HOST_WORKSPACE/artifact"
 export HOST_ARTIFACTS="$HOST_ARTIFACTS"
-export COPIED_PROJECT_PATH="$HOST_WORKSPACE/$DEFAULT_PROJECT_NAME"
+export COPIED_PROJECT_PATH="$HOST_WORKSPACE/$PROJECT_FOLDER_NAME"
 
 echo "Host workspace directory (copied project + logs, screenshots, etc.):"
 echo "  >>> $HOST_WORKSPACE"
@@ -50,8 +52,9 @@ chmod a+rw -R "$HOST_ARTIFACTS"
 rm -rf "$COPIED_PROJECT_PATH"
 rsync -aq --progress "$REPO" "$HOST_WORKSPACE" --exclude .git --exclude *.pyc --exclude .pytest_cache
 if [ $? -ne 0 ]; then
-  echo "Cant create workspace $COPIED_PROJECT_PATH, Please configure the path inside of this script"
+  echo "Can't create workspace '$COPIED_PROJECT_PATH', Please configure the path inside of this script"
   ls $HOST_WORKSPACE
+  return 1
 fi
 echo "$REPO is copied to $COPIED_PROJECT_PATH"
 echo "Entering the $COPIED_PROJECT_PATH directory"
@@ -76,7 +79,8 @@ python3 -m venv venv
 . venv/bin/activate
 
 BASE_REQ_FILE="$MODULE_PATH/requirements.txt"
-echo "Installing module requirements"
+echo "Installing module requirements..."
+echo ""
 python3 -m pip install --upgrade pip
 python3 -m pip install -r "$BASE_REQ_FILE"
 
